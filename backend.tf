fe7fb9d2193ff69d776ab88241b05e9f7595ef88
Douglas Martins <32877533+dougls@users.noreply.github.com>
2025-05-19 20:39:39 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "fiap-aula-terraform"
-#     key    = "aula08/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
-
 terraform {
   cloud {
-    organization = "fiap-soat-terraform"
+    organization = "soat-tf"
 
     workspaces {
-      name = "terraform-soat"
+      name = "production"
     }
   }
 }
